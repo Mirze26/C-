9e56eac207ea609a92815706b5a5c3963b07bb5e
unknown <mirzaab@code.edu.az>
2022-12-08 02:24:32 +0400

    C#

---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿//n - ədədinin 3-ə və 7-ə bölünüb-bölünməməsini tapın
+//int n = 21;
+//if (n%3==0)
+//{
+//}
+
+//if (n % 7 == 0)
+//{
+
+//    Console.WriteLine("Bolunur");
+//}
+//else
+//{
+//    Console.WriteLine("Bolunmur");
+//}
+
+
+
+//n və m ədədləri hər ikisi cütdürsə n və m ədədlərinin cəmini hesablayın
+//int n = 2;
+//int m = 8;
+//int result = 0;
+//if (n % 2 == 0) ;
+//{
+//}
+//if (m % 2 == 0) ;
+//{
+//}
+//int sum = (n + m);
+//Console.WriteLine(sum);
+
+
+//n ve m (n<m) ededleri arasindaki tek ededlerin cemini tapin.
+
+//int n = 1;
+//int m = 10;
+//int result = 0;
+//for (int i = n; i <= m; i++)
+//{
+//    if (i % 2 == 1)
+
+//    {
+//        result = result + i;
+
+//    }
+//}
+
+//Console.WriteLine(result);
+
+
+
+//arrayin icindeki tek ededlerin cemini tapin
+
+//int[] nums = { 1, 2, 3, 6, 7, 8, 9, 10, 11, 12, 13 };
+//int sum = 0;
+//for (int i = 0; i < nums.Length; i++)
+//{
+//	if (nums[i] % 2 == 1) ;
+//	{
+//		sum += nums[i];
+//	}
+//}
+//Console.WriteLine(sum);
+
+
+
+
+//arrayin icindeki cut ededlerin sayini tapin
+
+//int[] nums = { 2, 5, 8, 4, 6, 10 };
+//int count = 0;
+//for (int i = 0; i < nums.Length; i++)
+//{
+//	if (nums[i]%2==0)
+//	{
+//		count++;
+
+//	}
+
+//}
+//Console.WriteLine(count);
+
